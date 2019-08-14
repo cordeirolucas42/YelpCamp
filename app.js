d702b1c2f2f12552a61c13fc36b0d78e1ec4d568
cordeirolucas42 <cordeirolucas42@hotmail.com>
2019-08-14 02:56:06 +0000

    YelpCamp

---
+++ app.js
@@ -0,0 +1,99 @@
+// APP INITIALIZATION
+var express = require("express"); //import NPM express
+var app = express(); //initialize app with express
+var request = require("request"); //import NPM require
+var bodyParser = require("body-parser"); //import NPM body-parser
+app.use(bodyParser.urlencoded({extended: true})); //setting body-parser
+app.set("view engine", "ejs"); //setting ejs as standard
+
+//DATABASE INITIALIZATION
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/YelpCamp",{ useNewUrlParser: true });
+var campSchema = new mongoose.Schema({ //creating Schema
+   name: String,
+   image: String,
+   description: String});
+var Camp = mongoose.model("Camp", campSchema); //creating model and collection
+
+//CREATE INITIAL CAMPS
+// Camp.create({"name" : "Canto dos Pássaros", "image" : "https://media-cdn.tripadvisor.com/media/photo-s/05/cc/a4/95/canto-dos-passaros-hospedagem.jpg",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet."},
+// (err,camp)=>{
+// 	if(!err){
+// 		console.log(camp);
+// 	}
+// });
+// Camp.create({"name" : "Salmon Creek", "image" : "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet."},
+// (err,camp)=>{
+// 	if(!err){
+// 		console.log(camp);
+// 	}
+// });
+// Camp.create({"name" : "Mountain Goat's Rest", "image" : "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg",description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim ut sem viverra aliquet eget sit amet."},
+// (err,camp)=>{
+// 	if(!err){
+// 		console.log(camp);
+// 	}
+// });
+
+//LADING PAGE
+app.get("/", (req,res)=>{
+	//render views page
+	res.render("landing");
+});
+
+//INDEX - SHOW CAMP GROUNDS
+app.get("/campgrounds", (req,res)=>{
+	//access database and use it to render views page
+	Camp.find({},(err,campgrounds)=>{
+		if(!err){
+			//render views page
+			res.render("campgrounds",{campgrounds:campgrounds});
+		}
+	});	
+});
+
+//NEW - FORM TO MAKE A NEW CAMPGROUND
+app.get("/campgrounds/new", (req,res)=>{
+	//render views page
+	res.render("new");
+});
+
+//SHOW - PAGE FOR EACH CAMPGROUND
+app.get("/campgrounds/:id", (req,res)=>{
+	//get data from get request
+	var id = req.params.id;
+	//acces database to get specific campground
+	Camp.findById(id,(err,camp)=>{
+		if(!err){
+			res.render("show",{camp:camp});
+		}
+	});
+});
+
+//EDIT - FORM TO EDIT CAMPGROUND STILL NOT EDITING ANYTHING
+app.get("/campgrounds/:id/edit", (req,res)=>{
+	//get data from get request
+	var id = req.params.id;
+	//acces database to get specific campground
+	Camp.findById(id,(err,camp)=>{
+		if(!err){
+			res.render("edit",{camp:camp});
+		}
+	});
+})
+
+//CREATE - CREATE NEW CAMP GROUNDS
+app.post("/campgrounds", (req,res)=>{
+	//get data from post request
+	var newCampground = req.body.camp;
+	//insert new campground in the database
+	Camp.create(newCampground,(err,camp)=>{
+		if(!err){
+			//redirect to get /campgrounds
+			res.redirect("/campgrounds");
+		}
+	});	
+});
+
+// START SERVER
+app.listen(3000);
